f141602da5b50dd19459785eb52829422873f915
Christine Wong <jumiryoo@gmail.com>
2023-09-19 17:53:17 -0700

    added a missing line - type of cheese

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -19,6 +19,7 @@ function Show (props){
             <Form method="post" action={`/update/${post.id}`}>
                 <input type="text" name="name" placeholder="Name" defaultValue={post.name}/>
                 <input type="text" name="origin_country" placeholder="Origin Country" defaultValue={post.origin_country}/>
+                <input type="text" name="type" placeholder="Type" defaultValue={post.type}/>
                 <button>Update Cheese</button>
             </Form>
             <Form method="post" action={`/delete/${post.id}`}>
